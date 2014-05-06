247bbaa50c3763b183d2f55d58b62ec3774e2a32
Alex Speller <alex@alexspeller.com>
2014-05-06 04:12:44 +0100

    Template path

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module EmberTest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.ember.ember_path = "app/assets/javascripts/emberapp"
+    config.handlebars.templates_root = 'emberapp/templates'
   end
 end
